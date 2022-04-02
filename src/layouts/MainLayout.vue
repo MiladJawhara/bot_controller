dabd8f41c420c6d459b374f5d690173b30cd2659
Milad Jawhara <milad.jawhara@gmail.com>
2022-04-02 20:09:19 +0300

    -Project Clining

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          {{ $t('Bot Controler') }}
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header> {{ $t('Main Menu') }} </q-item-label>
+
+        <StandardLink v-for="link in links" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import StandardLink from 'src/components/StandardLink.vue';
+
+const linksList = [
+  {
+    title: 'Users',
+    caption: 'Bot Users List',
+    icon: 'group',
+    link: '/users',
+  },
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    StandardLink,
+  },
+
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      links: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+});
+</script>
---
+++ src/pages/UsersList.vue
@@ -0,0 +1,16 @@
+<template>
+  <h2>Users List</h2>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'UsersList',
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
